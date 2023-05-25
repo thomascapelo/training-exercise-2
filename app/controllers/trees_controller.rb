558058970766c57a0fb0f726a530b083443a095c
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-05-25 15:40:51 +0200

    Built the create method and view

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -4,5 +4,23 @@ class TreesController < ApplicationController
         @trees = Tree.all
     end 
 
-    
+    def new
+        @tree = Tree.new
+    end
+
+    def create
+        @tree = Tree.new(tree_params)
+        @tree.user = current_user
+        if @tree.save
+            redirect_to tree_path(@trees)
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def tree_params
+        params.require(:tree).permit(:name, :address, :description, :fruit_type, :price_per_year, :quantity_per_year)
+    end
 end
